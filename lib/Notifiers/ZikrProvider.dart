77e7b23004086b56d2e2545759f0e610518aa8e0
Kaan Binen <kaan.mlnym@hotmail.com>
2024-08-22 20:52:24 +0300

    update

--- lib/Notifiers/ZikrProvider.dart
+++ lib/Notifiers/ZikrProvider.dart
@@ -8,90 +8,93 @@ class ZikrProvider with ChangeNotifier {
   final List<Map<String, dynamic>> _nameJsonList = [];
   String? _selectedZikrName;
 
-  UnmodifiableListView<Map<String, dynamic>> get items => UnmodifiableListView([..._nameJsonList]);
+  UnmodifiableListView<Map<String, dynamic>> get items =>
+      UnmodifiableListView(_nameJsonList);
+
   String? get selectedZikrName => _selectedZikrName;
-  Map<String, dynamic> get selectedZikr => _nameJsonList.firstWhere((element) => element['name'] == _selectedZikrName);
 
-  void addZikr(String newName)
-  {
-    if (!_nameJsonList.any((element) => element['name'] == newName)) {
-      _nameJsonList.add({
-        "name": newName,
-        "count": 0
-      });
-      _saveToSharedPreferences();
-      notifyListeners();
-    }
+  Map<String, dynamic> get selectedZikr {
+    return _nameJsonList.firstWhere(
+          (element) => element['name'] == _selectedZikrName,
+      orElse: () => {'name': 'No Zikr Selected', 'count': 0},
+    );
   }
 
-  void updateZikr({required String zikrName, required int count})
-  {
-    final itemIndex = _nameJsonList.indexWhere((item) => item['name'] == zikrName);
-    if (itemIndex!= -1) {
-      _nameJsonList[itemIndex]['count'] = count;
-    } else {
-      print('Item with name $zikrName not found');
-    }
-    _saveToSharedPreferences();
+  Future<void> addZikr(String newName) async {
+    if (_nameJsonList.any((element) => element['name'] == newName)) return;
+
+    _nameJsonList.add({
+      "name": newName,
+      "count": 0
+    });
+    await _saveToSharedPreferences();
     notifyListeners();
   }
 
-  void removeZikr(String zikrName) {
+  Future<void> updateZikr({required String zikrName, required int count}) async {
     final itemIndex = _nameJsonList.indexWhere((item) => item['name'] == zikrName);
-    if (itemIndex!= -1) {
-      _nameJsonList.removeAt(itemIndex);
-      selectZikr(null);
-    } else {
+    if (itemIndex == -1) {
       print('Item with name $zikrName not found');
+      return;
     }
-    _saveToSharedPreferences();
+
+    _nameJsonList[itemIndex]['count'] = count;
+    await _saveToSharedPreferences();
     notifyListeners();
   }
 
-  void selectZikr(String? zikrName) {
-    if (zikrName!= null && _nameJsonList.any((element) => element['name'] == zikrName)) {
+  Future<void> removeZikr(String zikrName) async {
+    final itemIndex = _nameJsonList.indexWhere((item) => item['name'] == zikrName);
+    if (itemIndex == -1) {
+      print('Item with name $zikrName not found');
+      return;
+    }
+
+    _nameJsonList.removeAt(itemIndex);
+    if (_selectedZikrName == zikrName) {
+      await selectZikr(null);
+    }
+    await _saveToSharedPreferences();
+    notifyListeners();
+  }
+
+  Future<void> selectZikr(String? zikrName) async {
+    if (zikrName != null && _nameJsonList.any((element) => element['name'] == zikrName)) {
       _selectedZikrName = zikrName;
     } else if (_nameJsonList.isNotEmpty) {
       _selectedZikrName = _nameJsonList[0]['name'];
     } else {
       _selectedZikrName = null;
     }
-    _saveToSharedPreferences();
+    await _saveToSharedPreferences();
     notifyListeners();
   }
 
   Future<void> _saveToSharedPreferences() async {
-    final jsonEncoder = JsonEncoder();
-    final jsonString = jsonEncoder.convert(_nameJsonList);
-    SharedPreferences.getInstance().then((prefs) {
-      prefs.setString('zikrList', jsonString);
-      prefs.setString('selectedZikrName', _selectedZikrName?? '');
-    });
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final jsonString = jsonEncode(_nameJsonList);
+      await prefs.setString('zikrList', jsonString);
+      await prefs.setString('selectedZikrName', _selectedZikrName ?? '');
+    } catch (e) {
+      print('Error saving data to shared preferences: $e');
+    }
   }
-
 
   Future<void> _loadFromSharedPreferences() async {
     try {
       final prefs = await SharedPreferences.getInstance();
       final zikrListString = prefs.getString('zikrList');
-      if (zikrListString!= null) {
+      if (zikrListString != null) {
+        final List<dynamic> decodedList = jsonDecode(zikrListString);
         _nameJsonList.clear();
-        try {
-          _nameJsonList.addAll((jsonDecode(zikrListString) as List)
-              .map((e) => e as Map<String, dynamic>)
-              .toList());
-        } catch (e) {
-          print('Error decoding JSON: $e');
-        }
+        _nameJsonList.addAll(decodedList.map((e) => e as Map<String, dynamic>));
       }
       _selectedZikrName = prefs.getString('selectedZikrName');
-
-      notifyListeners();
     } catch (e) {
       print('Error loading data from shared preferences: $e');
-    } finally {
-      notifyListeners();
     }
+    notifyListeners();
   }
 
   Future<void> init() async {
